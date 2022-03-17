5e9b37362c4b7ce992ec3b0e92143c48e326e6dc
Skylagga <mike4797@icloud.com>
2022-03-17 11:23:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int amount = 4000;
+        int miles = 20;
+        int bonus = amount / miles;
+        System.out.println("Миль зачисленно: " + bonus);
+
+
+    }
+}
